3986a24340bae42a32fbe0415e6fe07409862020
Navjot Singh <navsingh16@publicisgroupe.net>
2020-05-20 23:01:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+      
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'file-upload-poc';
+   myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+    
+  constructor(private http: HttpClient) { }
+      
+  get f(){
+    return this.myForm.controls;
+  }
+     
+  onFileChange(event) {
+  
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.myForm.patchValue({
+        fileSource: file
+      });
+    }
+  }
+     
+  submit(){
+    const formData = new FormData();
+    formData.append('file', this.myForm.get('fileSource').value);
+   
+    this.http.post('http://localhost:8001/upload.php', formData)
+      .subscribe(res => {
+        console.log(res);
+        alert('Uploaded Successfully.');
+      })
+  }
 }
